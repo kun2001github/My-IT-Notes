5335d63c2b28e7e0cdaa9a251f8a80540bf76dff
kun2001github <1175553187@qq.com>
2024-09-02 22:41:07 +0800

    新增结束脚本并删除自身

--- Vscode格式化脚本.bat
+++ Vscode格式化脚本.bat
@@ -12,10 +12,10 @@ title  坤坤工具20240829
 ::-------------------------------------------------------------------------------------------------------------------
 :input_list
 echo.
-echo ****************************
+echo ************【菜单】****************
 echo ***1. 格式化Vscode数据***
 echo ***2. 安装Vscode1.85.2***
-echo *****************************
+echo ******* **【结束输入0】*************
 echo.
 set /p num=【请输入对应的数字】:
 if %num%==0 goto end
@@ -24,22 +24,29 @@ if %num%==2 goto installVscode
 echo 输入有误，请重新输入！
 goto input_list
 ::-------------------------------------------------------------------------------------------------------------------
+REM 关闭Code.exe 相关的进程并 重命名 .vscode 文件夹 以及重命名 Code 文件夹cmd
 :formatted_data
 echo.
-:: 重命名 .vscode 文件夹 以及重命名 Code 文件夹
+taskkill /im vscode.exe /f
+if exist "%USERPROFILE%\.vscode.bak" echo 注意：.vscode 已被备份为.vscode.bak
+if exist "%USERPROFILE%\AppData\Roaming\Code.bak" echo 注意：Code 已被备份为Code.bak
 rename "%USERPROFILE%\.vscode" ".vscode.bak"
 rename "%USERPROFILE%\AppData\Roaming\Code" "Code.bak"
-echo 操作完成... && pause
+echo 操作完成... && pause "按下回车键放返回【菜单】..."
 goto input_list
 ::----------------------------------------------------------------------------------------------------------------------
+rem 下载v1.85.2的便携版（绿色版）解压安装打开
 :installVscode
 echo.
-::下载v1.85.2的便携版（绿色版）解压安装打开
 curl -O "https://vscode.download.prss.microsoft.com/dbazure/download/stable/8b3775030ed1a69b13e4f4c628c612102e30a681/VSCode-win32-x64-1.85.2.zip"
 echo https://update.code.visualstudio.com/1.85.2/win32-x64-archive/stable
 mkdir VScode
 tar -xf  VSCode-win32-x64-1.85.2.zip -C ".\Vscode"
 del VSCode-win32-x64-1.85.2.zip
 .\Vscode\Code.exe
-echo 下载并启动完成... && pause
+echo 下载并启动完成... && pause pause "按下回车键放返回【菜单】..."
 goto input_list
+::----------------------------------------------------------------------------------------------------------------------
+rem 结束脚本并删除自身
+:end
+del "%~f0"
